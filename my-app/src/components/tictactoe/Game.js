27aa08858e006f9b5ce96c658475a4770647c9b2
DESKTOP-B0A7QR3\Duy Tan <tannld@jobtest.vn>
2022-12-06 17:31:39 +0700

    update code

--- my-app/src/components/tictactoe/Game.js
+++ my-app/src/components/tictactoe/Game.js
@@ -1,8 +1,58 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
+import { calculateWinner } from "../../helpers";
+import Board from "./Board";
 import "./GameStyle.css";
 
+const initialState = {
+  board: Array(9).fill(null),
+  xIsNext: true,
+};
+
+const gameReducer = (state, action) => {
+  switch (action.type) {
+    case "CLICK":
+      const { board, xIsNext } = state;
+      const { index, winner } = action.payload;
+      if (winner || board[index]) return state.board;
+      const nextState = JSON.parse(JSON.stringify(state));
+      nextState.board[index] = xIsNext ? "X" : "O";
+      nextState.xIsNext = !xIsNext;
+      return nextState;
+    default:
+      break;
+  }
+};
+
 const Game = () => {
-  return <div>Game</div>;
+  // const [board, setBoard] = useState(Array(9).fill(null));
+  // const [xIsNext, setXIsNext] = useState(true);
+
+  const [state, dispatch] = useReducer(gameReducer, initialState);
+
+  const winner = calculateWinner(state.board);
+
+  const handleClick = (index) => {
+    // const boardCopy = [...board];
+    // if (winner || boardCopy[index]) return;
+    // boardCopy[index] = xIsNext ? "X" : "O";
+    // setBoard(boardCopy);
+    // setXIsNext(!xIsNext);
+    dispatch({ type: "CLICK", payload: { index, winner } });
+  };
+
+  const handleResetGame = () => {
+    // setBoard(Array(9).fill(null));
+    // setXIsNext(true);
+  };
+  return (
+    <div>
+      <Board cells={state.board} onClick={handleClick}></Board>
+      {winner && <div className="game-winner">Winner is {winner}</div>}
+      <button className="game-reset" onClick={handleResetGame}>
+        Reset game
+      </button>
+    </div>
+  );
 };
 
 export default Game;
