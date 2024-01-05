1f649557b3f8758dc55ff90d16b957eaec243d32
DESKTOP-B0A7QR3\Duy Tan <tannld@jobtest.vn>
2024-01-05 13:21:19 +0700

    monkey blogging start

---
+++ monkey-blogging/src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./contexts/auth-context";
+import SignUpPage from "./pages/SignUpPage";
+
+function App() {
+    return (
+        <>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/sign-up" element={<SignUpPage></SignUpPage>}></Route>
+                </Routes>
+            </AuthProvider>
+        </>
+    );
+}
+
+export default App;
---
+++ monkey-blogging/src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import App from "./App.jsx";
+import "./styles/index.scss";
+import { theme } from "./utils/constants";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ThemeProvider>
+    </React.StrictMode>
+);
