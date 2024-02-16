bff053ff9f8fdc247709d4e8c576db2468b4548e
duytan <nlduytanhcm@gmail.com>
2024-02-16 19:45:22 +0700

    update code

--- monkey-blogging/src/module/post/PostAddNew.jsx
+++ monkey-blogging/src/module/post/PostAddNew.jsx
@@ -6,64 +6,126 @@ import { useForm } from "react-hook-form";
 import { Button } from "../../components/button";
 import { Radio } from "../../components/checkbox";
 import { Dropdown } from "../../components/dropdown";
+import slugify from "slugify";
+import { postStatus } from "../../utils/constants";
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import ImageUpload from "../../components/image/ImageUpload";
+import { useState } from "react";
+
+const storage = getStorage();
 
 const PostAddNewStyles = styled.div``;
 
 const PostAddNew = () => {
-  const { control, watch } = useForm({
+  const { control, watch, handleSubmit, setValue } = useForm({
     mode: "onChange",
-    defaultValues: { status: "", category: "" },
+    defaultValues: { title: "", slug: "", status: postStatus.PENDING, category: "" },
   });
 
   const watchStatus = watch("status");
   // const watchCategory = watch("category");
+  const addPostHandler = async (values) => {
+    const cloneValues = { ...values };
+    cloneValues.slug = slugify(values.slug || values.title);
+    cloneValues.status = Number(values.status);
+    handleUploadImage(cloneValues.image);
+    console.log("🚀 ~ addPostHandler ~ values:", cloneValues);
+  };
+
+  const [progress, setProgress] = useState(0);
+  const [image, setImage] = useState("");
+  const handleUploadImage = (file) => {
+    const storageRef = ref(storage, "images/" + file.name);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progressPercent = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setProgress(progressPercent);
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+        }
+      },
+      (error) => {
+        console.log("Error", error);
+        switch (error.code) {
+          case "storage/unauthorized":
+            break;
+          case "storage/canceled":
+            break;
+          case "storage/unknown":
+            break;
+        }
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImage(downloadURL);
+          console.log("File available at", downloadURL);
+        });
+      }
+    );
+  };
+
+  const onSelectImage = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+    setValue("image", file);
+  };
+
   return (
     <PostAddNewStyles>
       <h1 className="dashboard-heading">Add new post</h1>
-      <form>
+      <form onSubmit={handleSubmit(addPostHandler)}>
         <div className="grid grid-cols-2 mb-10 gap-x-10">
           <Field>
             <Label>Title</Label>
-            <Input
-              control={control}
-              placeholder="Enter your title"
-              name="title"
-            ></Input>
+            <Input control={control} placeholder="Enter your title" name="title" required></Input>
           </Field>
           <Field>
             <Label>Slug</Label>
-            <Input
-              control={control}
-              placeholder="Enter your slug"
-              name="slug"
-            ></Input>
+            <Input control={control} placeholder="Enter your slug" name="slug"></Input>
           </Field>
         </div>
         <div className="grid grid-cols-2 mb-10 gap-x-10">
+          <Field>
+            <Label>Image</Label>
+            <ImageUpload
+              name="image"
+              onChange={onSelectImage}
+              progress={progress}
+              image={image}
+            ></ImageUpload>
+          </Field>
           <Field>
             <Label>Status</Label>
             <div className="flex items-center gap-x-5">
               <Radio
                 name="status"
                 control={control}
-                checked={watchStatus === "approved"}
-                value="approved"
+                checked={watchStatus === postStatus.APPROVED}
+                value={postStatus.APPROVED}
               >
                 Approved
               </Radio>
               <Radio
                 name="status"
                 control={control}
-                checked={watchStatus === "pending"}
-                value="pending"
+                checked={watchStatus === postStatus.PENDING}
+                value={postStatus.PENDING}
               >
                 Pending
               </Radio>
               <Radio
                 name="status"
                 control={control}
-                checked={watchStatus === "reject"}
-                value="reject"
+                checked={watchStatus === postStatus.REJECTED}
+                value={postStatus.REJECTED}
               >
                 Reject
               </Radio>
@@ -71,11 +133,7 @@ const PostAddNew = () => {
           </Field>
           <Field>
             <Label>Author</Label>
-            <Input
-              name="author"
-              control={control}
-              placeholder="Find the author"
-            ></Input>
+            <Input name="author" control={control} placeholder="Find the author"></Input>
           </Field>
         </div>
         <div className="grid grid-cols-2 mb-10 gap-x-10">
