fa0ee06ed994ec85113fe209ea4bfe14e797030d
DESKTOP-B0A7QR3\Duy Tan <tannld@jobtest.vn>
2024-02-27 17:33:24 +0700

    call hot posts

--- monkey-blogging/src/module/home/HomeFeature.jsx
+++ monkey-blogging/src/module/home/HomeFeature.jsx
@@ -1,22 +1,38 @@
 import styled from "styled-components";
 import PostFeatureItem from "../post/PostFeatureItem";
 import Heading from "../../components/layout/Heading";
+import { collection, getDocs, limit, query, where } from "firebase/firestore";
+import { db } from "../../firebase/firebase-config";
+import { useEffect, useState } from "react";
 
 const HomeFeatureStyles = styled.div``;
 const HomeFeature = () => {
-  return (
-    <HomeFeatureStyles className="home-block">
-      <div className="container">
-        <Heading>Featured posts</Heading>
-        <div className="grid-layout">
-          <PostFeatureItem></PostFeatureItem>
-          <PostFeatureItem></PostFeatureItem>
-          <PostFeatureItem></PostFeatureItem>
-          <PostFeatureItem></PostFeatureItem>
-        </div>
-      </div>
-    </HomeFeatureStyles>
-  );
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        async function fetchPosts() {
+            const q = query(collection(db, "posts"), where("status", "==", 1), where("hot", "!=", true), limit(3));
+            const querySnapshot = await getDocs(q);
+            const results = [];
+            querySnapshot.forEach((doc) => {
+                results.push({ id: doc.id, ...doc.data() });
+            });
+            setPosts(results);
+        }
+        fetchPosts();
+    }, []);
+    if (posts.length <= 0) return;
+    return (
+        <HomeFeatureStyles className="home-block">
+            <div className="container">
+                <Heading>Featured posts</Heading>
+                <div className="grid-layout">
+                    {posts.map((post) => (
+                        <PostFeatureItem key={post.id} data={post}></PostFeatureItem>
+                    ))}
+                </div>
+            </div>
+        </HomeFeatureStyles>
+    );
 };
 
 export default HomeFeature;
--- monkey-blogging/src/module/post/PostFeatureItem.jsx
+++ monkey-blogging/src/module/post/PostFeatureItem.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import styled from "styled-components";
 import PostCategory from "./PostCategory";
 import PostTitle from "./PostTitle";
@@ -5,72 +6,68 @@ import PostMeta from "./PostMeta";
 import PostImage from "./PostImage";
 
 const PostFeatureItemStyles = styled.div`
-  width: 100%;
-  border-radius: 16px;
-  position: relative;
-  height: 169px;
-  .post {
-    &-image {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-      border-radius: 16px;
-    }
-    &-overlay {
-      position: absolute;
-      inset: 0;
-      border-radius: 16px;
-      background-color: rgba(0, 0, 0, 0.75);
-      mix-blend-mode: multiply;
-      opacity: 0.6;
-    }
-    &-content {
-      position: absolute;
-      inset: 0;
-      z-index: 10;
-      padding: 20px;
-      color: white;
-    }
-    &-top {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      margin-bottom: 16px;
-    }
-  }
-
-  @media screen and (min-width: 1024px) {
-    height: 272px;
-  }
-  @media screen and (max-width: 1023.98px) {
+    width: 100%;
+    border-radius: 16px;
+    position: relative;
+    height: 169px;
     .post {
-      &-content {
-        padding: 15px;
-      }
+        &-image {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            border-radius: 16px;
+        }
+        &-overlay {
+            position: absolute;
+            inset: 0;
+            border-radius: 16px;
+            background-color: rgba(0, 0, 0, 0.75);
+            mix-blend-mode: multiply;
+            opacity: 0.6;
+        }
+        &-content {
+            position: absolute;
+            inset: 0;
+            z-index: 10;
+            padding: 20px;
+            color: white;
+        }
+        &-top {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            margin-bottom: 16px;
+        }
     }
-  }
+
+    @media screen and (min-width: 1024px) {
+        height: 272px;
+    }
+    @media screen and (max-width: 1023.98px) {
+        .post {
+            &-content {
+                padding: 15px;
+            }
+        }
+    }
 `;
 
-const PostFeatureItem = () => {
-  return (
-    <PostFeatureItemStyles>
-      <PostImage
-        url="https://images.unsplash.com/photo-1553877522-43269d4ea984?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="unsplash"
-      ></PostImage>
-      <div className="post-overlay"></div>
-      <div className="post-content">
-        <div className="post-top">
-          <PostCategory>Kiến thức</PostCategory>
-          <PostMeta></PostMeta>
-        </div>
-        <PostTitle size="big">
-          Work Space cho người mới đi làm hiệu quả đến bất ngờ mà bạn không
-          tưởng tượng được đâu
-        </PostTitle>
-      </div>
-    </PostFeatureItemStyles>
-  );
+const PostFeatureItem = ({ data }) => {
+    if (!data || !data.id) return;
+
+    return (
+        <PostFeatureItemStyles>
+            <PostImage url={data.image} alt="unsplash"></PostImage>
+            <div className="post-overlay"></div>
+            <div className="post-content">
+                <div className="post-top">
+                    <PostCategory>Kiến thức</PostCategory>
+                    <PostMeta></PostMeta>
+                </div>
+                <PostTitle size="big">{data.title}</PostTitle>
+            </div>
+        </PostFeatureItemStyles>
+    );
 };
 
 export default PostFeatureItem;
